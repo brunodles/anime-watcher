fbaa7a2cb4dc998be94fa45aa5b355606fc32378
bruno <bruno.silva@concrete.com.br>
2017-05-31 13:42:54 -0300

    Update App to include NextEpisodes

--- app/src/main/kotlin/brunodles/animewatcher/MainActivity.kt
+++ app/src/main/kotlin/brunodles/animewatcher/MainActivity.kt
@@ -7,11 +7,15 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.widget.ImageView
 import bruno.animewatcher.explorer.AnimeExplorer
+import bruno.animewatcher.explorer.EpisodeLink
 import brunodles.animacurse.AnimaCurseFactory
 import brunodles.animesproject.AnimesProjectFactory
 import brunodles.animewatcher.databinding.ActivityMainBinding
+import brunodles.animewatcher.databinding.ItemEpisodeBinding
 import brunodles.anitubex.AnitubexFactory
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
@@ -22,6 +26,7 @@ import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
+import com.squareup.picasso.Picasso
 import java.util.*
 
 
@@ -34,22 +39,40 @@ class MainActivity : AppCompatActivity() {
 
     var binding: ActivityMainBinding? = null
     var player: SimpleExoPlayer? = null
+    var adapter: GenericAdapter<EpisodeLink, ItemEpisodeBinding>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        createPlayer();
+        createPlayer()
+
+        val manager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding?.nextEpisodes?.layoutManager = manager
+        adapter = GenericAdapter<EpisodeLink, ItemEpisodeBinding>(R.layout.item_episode) { viewHolder, item, index ->
+            viewHolder.binder.description.text = item.description
+            loadImageInto(item.image, viewHolder.binder.image)
+        }
+        binding?.nextEpisodes?.adapter = adapter
+    }
+
+    private fun loadImageInto(url: String?, image: ImageView) {
+        if (url == null) return
+        Picasso.Builder(this).indicatorsEnabled(true)
+                .loggingEnabled(true)
+                .build()
+                .load(url)
+                .placeholder(R.drawable.loading)
+                .error(R.drawable.error)
+                .into(image)
     }
 
     private fun createPlayer() {
-        // 1. Create a default TrackSelector
         val mainHandler = Handler()
         val bandwidthMeter = DefaultBandwidthMeter()
         val videoTrackSelectionFactory = AdaptiveTrackSelection.Factory(bandwidthMeter)
         val trackSelector = DefaultTrackSelector(videoTrackSelectionFactory)
 
-        // 2. Create the player
         val player = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
         binding?.player?.player = player
     }
@@ -60,6 +83,8 @@ class MainActivity : AppCompatActivity() {
             val currentEpisode = it.currentEpisode()
             prepareVideo(currentEpisode.video)
             binding?.title?.text = currentEpisode.description
+
+            adapter?.list = it.nextEpisodes()
 
 //            binding?.text?.text = it
             // share to another player
