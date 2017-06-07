38061eb5ffdabeb82da3ed77f833519cc44f19dc
Bruno de Lima <dlimaun@gmail.com>
2017-06-07 09:24:02 -0300

    Update episode link to add default value for image parameter

--- explorer/src/main/kotlin/bruno/animewatcher/explorer/EpisodeLink.kt
+++ explorer/src/main/kotlin/bruno/animewatcher/explorer/EpisodeLink.kt
@@ -3,4 +3,4 @@ package bruno.animewatcher.explorer
 import brunodles.kotlin.annotation.NoArgs
 
 @NoArgs
-data class EpisodeLink(val link: String, val description: String, val image: String?)
+data class EpisodeLink(val link: String, val description: String, val image: String? = null)
