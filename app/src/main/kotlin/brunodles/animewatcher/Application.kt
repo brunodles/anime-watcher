d9f5d84e7a7f0a4da1022172da35ebe96b2f0327
Bruno de Lima <dlimaun@gmail.com>
2017-09-03 09:11:18 -0300

    Add Crashlytics

--- app/src/main/kotlin/brunodles/animewatcher/Application.kt
+++ app/src/main/kotlin/brunodles/animewatcher/Application.kt
@@ -2,10 +2,13 @@ package brunodles.animewatcher
 
 import android.app.Application
 import com.google.firebase.database.FirebaseDatabase
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 class Application : Application() {
     override fun onCreate() {
         super.onCreate()
         FirebaseDatabase.getInstance().setPersistenceEnabled(true)
+        Fabric.with(this, Crashlytics())
     }
 }
