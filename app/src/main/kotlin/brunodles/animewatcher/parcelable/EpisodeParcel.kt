40b01ea1b2d6b422a171cc1aecace487363680a5
Bruno de Lima <dlimaun@gmail.com>
2017-12-10 11:01:41 -0200

    Create Episode Parceler

---
+++ app/src/main/kotlin/brunodles/animewatcher/parcelable/EpisodeParcel.kt
@@ -0,0 +1,60 @@
+package brunodles.animewatcher.parcelable
+
+import android.os.Parcel
+import android.os.Parcelable
+import brunodles.animewatcher.explorer.Episode
+import brunodles.kotlin.annotation.NoArgs
+import java.io.Serializable
+
+data class EpisodeParcel(
+        val description: String,
+        val number: Int,
+        val animeName: String? = null,
+        val image: String? = null,
+        val video: String? = null,
+        val link: String? = null,
+        val nextEpisodes: List<EpisodeParcel>? = arrayListOf()) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readInt(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.createTypedArrayList(CREATOR))
+
+    constructor(episode: Episode): this(
+            episode.description,
+            episode.number,
+            episode.animeName,
+            episode.image,
+            episode.video,
+            episode.link,
+            episode.nextEpisodes?.map { EpisodeParcel(it) }?.toList()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(description)
+        parcel.writeInt(number)
+        parcel.writeString(animeName)
+        parcel.writeString(image)
+        parcel.writeString(video)
+        parcel.writeString(link)
+        parcel.writeTypedList(nextEpisodes)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<EpisodeParcel> {
+        override fun createFromParcel(parcel: Parcel): EpisodeParcel {
+            return EpisodeParcel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<EpisodeParcel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
