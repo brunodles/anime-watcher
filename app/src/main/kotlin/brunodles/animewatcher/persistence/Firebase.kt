f67ad01e1a76b2d0c723fa0ee8b96a3ba2bc7437
Bruno de Lima <dlimaun@gmail.com>
2017-12-27 18:36:59 -0200

    Improve Firebase history function

--- app/src/main/kotlin/brunodles/animewatcher/persistence/Firebase.kt
+++ app/src/main/kotlin/brunodles/animewatcher/persistence/Firebase.kt
@@ -1,10 +1,15 @@
 package brunodles.animewatcher.persistence
 
+import android.util.Log
 import brunodles.animewatcher.BuildConfig
 import brunodles.animewatcher.explorer.Episode
+import brunodles.rxfirebase.observableChildAdded
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.FirebaseDatabase
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.subscribeBy
+import java.util.concurrent.TimeUnit
 
 object Firebase {
 
@@ -12,6 +17,7 @@ object Firebase {
     private val REF_USERS = "users"
     private val REF_HISTORY = "history"
     private val REF_NUMBER = "number"
+    private val EMPTY_URL = ""
 
     /** Return the reference to all videos mapped by the firebase **/
     fun videosRef() = firebaseRef().child(REF_VIDEO)
@@ -26,7 +32,22 @@ object Firebase {
 
     fun addToHistory(url: String) {
         val currentUser = FirebaseAuth.getInstance().currentUser ?: return
-        history(currentUser).push().setValue(url)
+
+        val history = Firebase.history(currentUser)
+        history.limitToLast(1)
+                .orderByKey()
+                .observableChildAdded(String::class.java)
+                .timeout(5, TimeUnit.SECONDS, Observable.just(EMPTY_URL))
+                .first(EMPTY_URL)
+                .subscribeBy(
+                        onSuccess = {
+                            if (url != it)
+                                history.push().setValue(url)
+                        },
+                        onError = {
+                            Log.d("Firebase", "addToHistory: failed to add to history", it)
+                        }
+                )
     }
 
     fun history(currentUser: FirebaseUser) =
