14fce3f343ff20a31b1ffc1331ed109e0aab4c1f
Bruno de Lima <dlimaun@gmail.com>
2017-12-29 12:25:34 -0200

    Update persistence.Preferences to add last animeImageUrl

--- app/src/main/kotlin/brunodles/animewatcher/persistence/Preferences.kt
+++ app/src/main/kotlin/brunodles/animewatcher/persistence/Preferences.kt
@@ -7,15 +7,26 @@ class Preferences(private val context: Context) {
 
     companion object {
         private val KEY_URL = "URL"
+        private val KEY_IMAGE_URL = "IMAGE_URL"
     }
 
-    fun setUrl(url:String) =
+    fun setUrl(url: String) =
             preferences()
                     .edit()
                     .putString(KEY_URL, url)
                     .apply()
 
     fun getUrl() = preferences().getString(KEY_URL, null)
+
+    fun setLastAnimeImage(url: String) =
+            preferences()
+                    .edit()
+                    .putString(KEY_IMAGE_URL, url)
+                    .apply()
+
+    fun getLastAnimeImage() =
+            preferences()
+                    .getString(KEY_IMAGE_URL, "")
 
     private fun preferences() = PreferenceManager.getDefaultSharedPreferences(context)
 
