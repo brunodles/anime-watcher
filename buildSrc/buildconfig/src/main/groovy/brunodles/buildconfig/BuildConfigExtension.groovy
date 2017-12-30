b4a48ed905b13a3ded83624218d6c03441529cc5
Bruno de Lima <dlimaun@gmail.com>
2017-12-30 18:32:55 -0200

    Move buildConfig to a file

--- buildSrc/buildconfig/src/main/groovy/brunodles/buildconfig/BuildConfigExtension.groovy
+++
@@ -1,9 +0,0 @@
-package brunodles.buildconfig
-
-class BuildConfigExtension {
-    HashMap<String, String> properties = new HashMap<>()
-
-    def void add(String key, String value) {
-        properties.put(key, value)
-    }
-}
