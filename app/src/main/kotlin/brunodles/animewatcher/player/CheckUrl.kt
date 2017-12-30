d22bb3f82d4aaeadde383c5527f816106550b6ed
Bruno de Lima <dlimaun@gmail.com>
2017-12-30 19:35:30 -0200

    Add AnimesProject

--- app/src/main/kotlin/brunodles/animewatcher/player/CheckUrl.kt
+++ app/src/main/kotlin/brunodles/animewatcher/player/CheckUrl.kt
@@ -3,6 +3,7 @@ package brunodles.animewatcher.player
 import android.content.Intent
 import android.util.Log
 import brunodles.animacurse.AnimaCurseFactory
+import brunodles.animesproject.AnimesProjectFactory
 import brunodles.animewatcher.explorer.Episode
 import brunodles.animewatcher.explorer.PageParserFactory
 
@@ -12,6 +13,7 @@ object CheckUrl {
 
     init {
         PageParserFactory.factories.add(AnimaCurseFactory)
+        PageParserFactory.factories.add(AnimesProjectFactory)
     }
 
     fun findUrl(intent: Intent): String? {
