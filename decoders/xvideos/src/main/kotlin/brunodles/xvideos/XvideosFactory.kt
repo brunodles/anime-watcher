34f2277e350134200a68c5f3b45ad1fed64a6be9
Gabriel Costa <gabrielcosta08@gmail.com>
2018-01-15 11:52:45 -0200

    Add xvideos decoder module

---
+++ decoders/xvideos/src/main/kotlin/brunodles/xvideos/XvideosFactory.kt
@@ -0,0 +1,18 @@
+package brunodles.animacurse
+
+import brunodles.animewatcher.explorer.Episode
+import brunodles.animewatcher.explorer.PageParser
+
+object XvideosFactory : PageParser {
+
+    override fun isEpisode(url: String): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun episode(url: String): Episode {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
+
