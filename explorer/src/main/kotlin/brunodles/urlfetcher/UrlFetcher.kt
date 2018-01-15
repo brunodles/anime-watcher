2f355318df8ae941d9faef507aa72235b7c32516
Bruno de Lima <dlimaun@gmail.com>
2018-01-14 23:49:37 -0200

    Update UrlFetcher to let redirect parametrized

--- explorer/src/main/kotlin/brunodles/urlfetcher/UrlFetcher.kt
+++ explorer/src/main/kotlin/brunodles/urlfetcher/UrlFetcher.kt
@@ -13,14 +13,17 @@ interface UrlFetcher {
         var cacheDir = BuildConfig.ROOT_DIR
         var useLog = false
 
-        fun fetcher(url: String): UrlFetcher {
-            var fetcher: UrlFetcher = JsoupFetcher(url)
+        @Deprecated("This does not support composable configuration", ReplaceWith("composableFetcher"))
+        fun fetcher(url: String): UrlFetcher = composableFetcher(url).withRedirect()
+
+        fun composableFetcher(url: String): UrlFetcherComposable {
+            val urlFetcherComposable = UrlFetcherComposable(url)
             if (useCache)
-                fetcher = CacheFetcher(url, fetcher)
-            fetcher = RedirectFetcher(fetcher)
-            return fetcher
+                urlFetcherComposable.withCache()
+            return urlFetcherComposable
         }
 
+        @Deprecated("This does not support composable configuration and always use get", ReplaceWith("composableFetcher"))
         fun fetchUrl(url: String): Document = fetcher(url).get()
     }
 }
