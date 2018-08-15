809536323163efcbf384efdbe4ff379929fcd28b
Bruno de Lima e Silva <dlimaun@gmail.com>
2018-08-15 17:11:45 -0300

    Add parameter validation to launchOnDevice script

--- app/launchOnDevice.sh
+++ app/launchOnDevice.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-adb shell am start -a "android.intent.action.VIEW" -d "$1"
+if [ -z $1 ]; then
+    echo "Missing url parameter"
+    echo "Usage: $0 <wanted url>"
+    echo "Sample: $0 https://github.com/brunodles/anime-watcher"
+else
+    adb shell am start -a "android.intent.action.VIEW" -d "$1"
+fi
