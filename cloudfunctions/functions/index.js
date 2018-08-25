853428ebfa597331fdb33053e673973c6eb3127d
Bruno de Lima e Silva <dlimaun@gmail.com>
2018-08-25 12:20:09 -0300

    Add decoder as a recursive function

--- cloudfunctions/functions/index.js
+++ cloudfunctions/functions/index.js
@@ -6,22 +6,40 @@ client.on('error', function (err) {
     console.error('Something went wrong on the client', err);
 })
 
-const SERVER_URLS = ["https://anime-watcher-ktor.herokuapp.com/decoder",
-    "https://anime-watcher-spark.herokuapp.com/decoder"];
+const SERVER_URLS = [
+    "https://anime-watcher-spark.herokuapp.com/decoder",
+    "https://anime-watcher-ktor.herokuapp.com/decoder",
+];
 
-exports.helloWorld = functions.https.onRequest((req, res) => {
+exports.decoder = functions.https.onRequest((req, res) => {
     var url = Object.keys(req.body)[0];
-    console.log(url);
-    client.post(SERVER_URLS[0], url,  function (data, response) {
-        return res.status(200)
-            .type('application/json')
-            .send(data);
-    }).on('error', function (error) {
-        console.log("First server errored, trying next")
-        client.post(SERVER_URLS[1], url,  function (data, response) {
-                return res.status(200)
-                    .type('application/json')
-                    .send(data);
-            })
-    })
+    console.log("Requested url: ", url);
+    return decode(0, url, res)
 });
+
+function decode(serverIndex, url, response)  {
+
+    console.log("Server("+serverIndex+") - Request data: ", url);
+    client.post(SERVER_URLS[serverIndex], {data:url}, function (data, res) {
+        console.log("Server("+serverIndex+")");
+        console.log("\tStatusCode: ",res.statusCode);
+        console.log("\tData: ", data)
+
+        if (res.statusCode === 200) {
+            return response.status(200)
+                .type('application/json')
+                .send(data);
+        } else {
+            return decode(serverIndex+1, url, response);
+        }
+    }).on('error', function (error) {
+        if (serverIndex === SERVER_URLS.length){
+            return response.status(500)
+                .type('application/json')
+                .send(error)
+        } else {
+            console.log("Server("+serverIndex+") erred, trying next")
+            return decode(serverIndex+1, url, response);
+        }
+    })
+}
