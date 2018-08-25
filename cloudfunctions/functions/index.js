7b674695823eb85ea6d2e3ee5594037ce58ac8ee
Bruno de Lima e Silva <dlimaun@gmail.com>
2018-08-25 12:38:56 -0300

    Refactor cloudfunctions to a generic gateway

--- cloudfunctions/functions/index.js
+++ cloudfunctions/functions/index.js
@@ -7,39 +7,49 @@ client.on('error', function (err) {
 })
 
 const SERVER_URLS = [
-    "https://anime-watcher-spark.herokuapp.com/decoder",
-    "https://anime-watcher-ktor.herokuapp.com/decoder",
+    "https://anime-watcher-spark.herokuapp.com",
+    "https://anime-watcher-ktor.herokuapp.com",
 ];
 
-exports.decoder = functions.https.onRequest((req, res) => {
+exports.gateway = functions.https.onRequest((req, res) => {
     var url = Object.keys(req.body)[0];
-    console.log("Requested url: ", url);
-    return decode(0, url, res)
+    console.log("Requested body: ", url);
+    console.log("Requested url, ", req.url)
+    return decode(0, "POST", req.url, {data:url}, res)
 });
 
-function decode(serverIndex, url, response)  {
+function decode(serverIndex, method, path, body, response)  {
+    console.log("Server("+serverIndex+") - Request data: ", body);
 
-    console.log("Server("+serverIndex+") - Request data: ", url);
-    client.post(SERVER_URLS[serverIndex], {data:url}, function (data, res) {
+    var successHandler = function (data, res) {
         console.log("Server("+serverIndex+")");
         console.log("\tStatusCode: ",res.statusCode);
         console.log("\tData: ", data)
-
+    
         if (res.statusCode === 200) {
             return response.status(200)
                 .type('application/json')
                 .send(data);
         } else {
-            return decode(serverIndex+1, url, response);
+            return decode(serverIndex+1, method, path, body, response);
         }
-    }).on('error', function (error) {
+    }
+
+    var call;
+    if (method === "POST") {
+        call = client.post(SERVER_URLS[serverIndex]+path, body, successHandler)
+    } else {
+        call = client.get(SERVER_URLS[serverIndex]+path, successHandler)
+    }
+
+    call.on('error', function (error) {
         if (serverIndex === SERVER_URLS.length){
             return response.status(500)
                 .type('application/json')
                 .send(error)
         } else {
             console.log("Server("+serverIndex+") erred, trying next")
-            return decode(serverIndex+1, url, response);
+            return decode(serverIndex+1, method, path, body, response);
         }
     })
 }
