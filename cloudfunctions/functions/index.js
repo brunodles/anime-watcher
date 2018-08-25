51ba9094cf9b70abcda5e27a170708806f58b490
Bruno de Lima e Silva <dlimaun@gmail.com>
2018-08-25 13:00:19 -0300

    Fix error handler on cloudfunctions

--- cloudfunctions/functions/index.js
+++ cloudfunctions/functions/index.js
@@ -2,12 +2,12 @@ const functions = require('firebase-functions');
 
 const Client = require('node-rest-client').Client;
 const client = new Client();
-client.on('error', function (err) {
+client.on('error', (err) => {
     console.error('Something went wrong on the client', err);
 })
 
 const SERVER_URLS = [
-    "https://anime-watcher-spark.herokuapp.com",
+//    "https://anime-watcher-spark.herokuapp.com",
     "https://anime-watcher-ktor.herokuapp.com",
 ];
 
@@ -19,31 +19,11 @@ exports.gateway = functions.https.onRequest((req, res) => {
 });
 
 function decode(serverIndex, method, path, body, response)  {
-    console.log("Server("+serverIndex+") - Request data: ", body);
+    var url = SERVER_URLS[serverIndex]+path;
+    console.log("Server("+serverIndex+") = ", url);
 
-    var successHandler = function (data, res) {
-        console.log("Server("+serverIndex+")");
-        console.log("\tStatusCode: ",res.statusCode);
-        console.log("\tData: ", data)
-    
-        if (res.statusCode === 200) {
-            return response.status(200)
-                .type('application/json')
-                .send(data);
-        } else {
-            return decode(serverIndex+1, method, path, body, response);
-        }
-    }
-
-    var call;
-    if (method === "POST") {
-        call = client.post(SERVER_URLS[serverIndex]+path, body, successHandler)
-    } else {
-        call = client.get(SERVER_URLS[serverIndex]+path, successHandler)
-    }
-
-    call.on('error', function (error) {
-        if (serverIndex === SERVER_URLS.length){
+    var errorHandler = (error) => {
+        if (serverIndex + 1 >= SERVER_URLS.length){
             return response.status(500)
                 .type('application/json')
                 .send(error)
@@ -51,5 +31,27 @@ function decode(serverIndex, method, path, body, response)  {
             console.log("Server("+serverIndex+") erred, trying next")
             return decode(serverIndex+1, method, path, body, response);
         }
-    })
+    }
+
+    var successHandler = (data, res) => {
+        console.log("Server("+serverIndex+")");
+        console.log("\tStatusCode: ",res.statusCode);
+        console.log("\tData: ", data)
+
+        if (res.statusCode === 200) {
+            return response.status(200)
+                .type('application/json')
+                .send(data);
+        } else {
+            return errorHandler(data)
+        }
+    }
+
+    var call;
+    if (method === "POST") {
+        call = client.post(url, body, successHandler)
+    } else {
+        call = client.get(url, successHandler)
+    }
+    call.on('error', errorHandler)
 }
