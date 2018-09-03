eb74333202492d0d08a92031ebf5318b8b03b428
Bruno de Lima e Silva <dlimaun@gmail.com>
2018-09-03 14:22:45 -0300

    Add image attribute for samples on AnitubeSiteFactoryTest

--- explorer/src/test/kotlin/brunodles/animewatcher/decoders/AnitubeSiteFactoryTest.kt
+++ explorer/src/test/kotlin/brunodles/animewatcher/decoders/AnitubeSiteFactoryTest.kt
@@ -12,18 +12,20 @@ class AnitubeSiteFactoryTest {
         val VALID_URLS = arrayOf("https://www.anitube.site/765/")
         val INVALID_URLS = arrayOf("anitub")
         val currentEpisode = Episode(
-                number = 1,
-                description = "Eu Sou Ruffy!",
-                link = "https://www.anitube.site/765/",
-                video = "https://www.blogger.com/video-play.mp4?contentId=3eb2b428663ef38f",
-                animeName = "One Piece",
-                nextEpisodes = arrayListOf(
-                        Episode(number = 2,
-                                animeName = "One Piece",
-                                description = "O Grande Espadachim Aparece!",
-                                link = "https://www.anitube.site/802/"
-                        )
+            number = 1,
+            description = "Eu Sou Ruffy!",
+            link = "https://www.anitube.site/765/",
+            video = "https://www.blogger.com/video-play.mp4?contentId=3eb2b428663ef38f",
+            image = "https://www.anitube.site/player/img/Capa-Player-Cine.png",
+            animeName = "One Piece",
+            nextEpisodes = arrayListOf(
+                Episode(
+                    number = 2,
+                    animeName = "One Piece",
+                    description = "O Grande Espadachim Aparece!",
+                    link = "https://www.anitube.site/802/"
                 )
+            )
         )
     }
 
