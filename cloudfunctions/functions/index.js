7a241e5eda830b7f1913f219f016cdc6c756b388
Bruno de Lima e Silva <dlimaun@gmail.com>
2018-10-23 12:55:24 -0300

    Update cloudfunctions to listen to history updates

--- cloudfunctions/functions/index.js
+++ cloudfunctions/functions/index.js
@@ -1,6 +1,38 @@
 const functions = require('firebase-functions');
-const gatewayModule = require("./gateway")
-const searchModule = require("./search")
 
-exports.gateway = functions.https.onRequest(gatewayModule.gateway);
-exports.search = functions.https.onRequest(searchModule.search);
+//const searchModule = require("./search")
+
+const admin = require('firebase-admin');
+admin.initializeApp()
+
+const invalidTextRegex = /[^\d\w]+/g;
+
+
+//exports.search = functions.https.onRequest(searchModule.search);
+
+exports.onAddToHistory = functions.database
+    .ref("/users/{uid}/history/{pushId}")
+    .onCreate((snapshot, context) => {
+
+    const userId = context.auth.uid;
+    const episodeUrl = snapshot.val();
+    const episodeId = episodeUrl.replace(invalidTextRegex, "");
+
+    console.log("user: "+userId +", episodeUrl: "+episodeUrl+", episodeId: "+episodeId);
+
+    return admin.database().ref("/videos/"+episodeId).once("value").then( episodeRef => {
+
+        const episode = episodeRef.val();
+
+        console.log("episode: ", JSON.stringify(episode));
+
+        return admin.database().ref("/users/"+userId+"/historyLast").set(episode)
+            .then(res => {
+                if (episode.nextEpisodes !== undefined && episode.nextEpisodes.length > 0)
+                    return admin.database()
+                        .ref("/users/" + userId + "/next/" + episode.animeName)
+                        .set(episode.nextEpisodes[0]);
+                return res;
+        });
+    })
+});
