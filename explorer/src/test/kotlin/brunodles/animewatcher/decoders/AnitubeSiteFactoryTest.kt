2716cceabf61c2d0eb2b63eb3042b0203c57bb71
Bruno de Lima e Silva <dlimaun@gmail.com>
2018-10-29 18:16:32 -0300

    Extract responses from AnitubeSiteFactoryTest

--- explorer/src/test/kotlin/brunodles/animewatcher/decoders/AnitubeSiteFactoryTest.kt
+++ explorer/src/test/kotlin/brunodles/animewatcher/decoders/AnitubeSiteFactoryTest.kt
@@ -2,8 +2,12 @@ package brunodles.animewatcher.decoders
 
 import brunodles.animewatcher.explorer.Episode
 import brunodles.animewatcher.testhelper.FactoryChecker
+import brunodles.animewatcher.testhelper.FactoryChecker.whenCheckIsEpisode
+import brunodles.animewatcher.testhelper.FactoryChecker.whenEpisode
+import brunodles.loadEpisodeResource
 import com.greghaskins.spectrum.Spectrum
 import org.junit.runner.RunWith
+import resource_helper.Resources
 
 @RunWith(Spectrum::class)
 class AnitubeSiteFactoryTest {
@@ -11,25 +15,12 @@ class AnitubeSiteFactoryTest {
     companion object {
         val VALID_URLS = arrayOf("https://www.anitube.site/765/")
         val INVALID_URLS = arrayOf("anitub")
-        val currentEpisode = Episode(
-            number = 1,
-            description = "Eu Sou Ruffy!",
-            link = "https://www.anitube.site/765/",
-            video = "https://www.blogger.com/video-play.mp4?contentId=3eb2b428663ef38f",
-            image = "https://www.anitube.site/player/img/Capa-Player-Cine.png",
-            animeName = "One Piece",
-            nextEpisodes = arrayListOf(
-                Episode(
-                    number = 2,
-                    animeName = "One Piece",
-                    description = "O Grande Espadachim Aparece!",
-                    link = "https://www.anitube.site/802/"
-                )
-            )
-        )
     }
 
     init {
-        FactoryChecker.describeFactory(AnitubeSiteFactory, VALID_URLS, INVALID_URLS, currentEpisode)
+        FactoryChecker.describe(AnitubeSiteFactory) {
+            whenEpisode(Resources.anitubesite.playerEpisodesJson.loadEpisodeResource())
+            whenCheckIsEpisode(VALID_URLS, INVALID_URLS)
+        }
     }
 }
