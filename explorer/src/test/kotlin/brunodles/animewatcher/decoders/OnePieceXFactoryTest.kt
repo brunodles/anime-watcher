c0a4e662d70c46c9118de82888e2b59c9890e4f3
Bruno de Lima e Silva <dlimaun@gmail.com>
2018-10-29 18:17:01 -0300

    Extract responses from OnePieceXFactoryTest

--- explorer/src/test/kotlin/brunodles/animewatcher/decoders/OnePieceXFactoryTest.kt
+++ explorer/src/test/kotlin/brunodles/animewatcher/decoders/OnePieceXFactoryTest.kt
@@ -1,11 +1,12 @@
 package brunodles.animewatcher.decoders
 
-import brunodles.animewatcher.explorer.Episode
 import brunodles.animewatcher.testhelper.FactoryChecker
 import brunodles.animewatcher.testhelper.FactoryChecker.whenCheckIsEpisode
 import brunodles.animewatcher.testhelper.FactoryChecker.whenEpisode
+import brunodles.loadEpisodeResource
 import com.greghaskins.spectrum.Spectrum
 import org.junit.runner.RunWith
+import resource_helper.Resources
 
 @RunWith(Spectrum::class)
 class OnePieceXFactoryTest {
@@ -21,27 +22,11 @@ class OnePieceXFactoryTest {
             "https://one-piece-x.com.br/episodios/t04/",
             "one-piece-x.com.br/episodios/online"
         )
-
-        val currentEpisode = Episode(
-            number = 208,
-            animeName = "One Piece",
-            description = "Os piratas do Foxy!! A Davy Back!",
-            link = "https://one-piece-x.com.br/episodios/online/208/",
-            video = "https:\\/\\/\\w+.onepieceex\\.com\\.br\\/episodios\\/\\w+\\/OpEx_\\d+_\\w+.mp4\\?st=.*",
-            nextEpisodes = arrayListOf(
-                Episode(
-                    description = "Next",
-                    number = 209,
-                    animeName = "One Piece",
-                    link = "https://onepiece-ex.com.br/episodios/online/209"
-                )
-            )
-        )
     }
 
     init {
         FactoryChecker.describe(OnePieceXFactory) {
-            whenEpisode(currentEpisode)
+            whenEpisode(Resources.onepiecex.playerEpisodesJson.loadEpisodeResource())
             whenCheckIsEpisode(VALID_URLS, INVALID_URLS)
         }
     }
